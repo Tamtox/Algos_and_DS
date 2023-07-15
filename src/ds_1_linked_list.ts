1e66849e88cdbd1250470169356b8396c53c9d21
Tamtox <39272747@protonmail.com>
2023-07-15 14:31:36 +0600

    asd

--- src/ds_1_linked_list.ts
+++ src/ds_1_linked_list.ts
@@ -30,3 +30,69 @@ class ListNode {
 // Stack - linked list where elements are added and removed from the top.
 
 // Queue - linked list where elements are added to the back and removed from the front.
+
+// Definition for singly-linked list.
+class ListNodeEx {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(list1: ListNodeEx | null, list2: ListNodeEx | null): ListNodeEx | null {
+  if (!list1 && !list2) {
+    return null;
+  } else if (!list1) {
+    return list2;
+  } else if (!list2) {
+    return list1;
+  }
+  let resultHead: ListNodeEx | null = null;
+  let resultCurrent: ListNodeEx | null = resultHead;
+  while (list1.next !== null || list2.next !== null) {
+    const val1 = list1.val;
+    const val2 = list2.val;
+    const next1 = list1.next;
+    const next2 = list2.next;
+    if (val1 && val2) {
+      if (val1 >= val2) {
+        if (resultCurrent === null) {
+          resultCurrent = new ListNode(val2, null);
+          resultHead = resultCurrent;
+          resultCurrent.next = new ListNode(val1, null);
+          resultCurrent = resultCurrent.next;
+        } else {
+          resultCurrent.next = new ListNode(val2, null);
+          resultCurrent = resultCurrent.next;
+          list2 = list2.next;
+          resultCurrent.next = new ListNode(val1, null);
+          resultCurrent = resultCurrent.next;
+          list1 = list1.next;
+        }
+      } else {
+        if (resultCurrent === null) {
+          resultCurrent = new ListNode(val1, null);
+          resultHead = resultCurrent;
+          resultCurrent.next = new ListNode(val2, null);
+          resultCurrent = resultCurrent.next;
+        } else {
+          resultCurrent.next = new ListNode(val1, null);
+          resultCurrent = resultCurrent.next;
+          list1 = list1.next;
+          resultCurrent.next = new ListNode(val2, null);
+          resultCurrent = resultCurrent.next;
+          list2 = list2.next;
+        }
+      }
+    } else {
+      if (!val1 && !val2) {
+        break;
+      } else {
+        //
+      }
+    }
+  }
+  return resultHead;
+}
